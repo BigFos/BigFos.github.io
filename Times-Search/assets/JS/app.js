19df6f1624a58aea4a9ea54069c021b1c0d0fc84
BigFos <jfreak0506@yahoo.com>
2017-05-13 13:50:07 -0700

    updated api url in js

--- Times-Search/assets/JS/app.js
+++ Times-Search/assets/JS/app.js
@@ -1,5 +1,6 @@
 var apiKey = "664e215dd5a34926b033d389481d8095";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=cats";
+var searchTerm = "cats";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchTerm;
 
 $.ajax({
   url: queryURL,
