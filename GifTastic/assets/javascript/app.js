f0f25dfae40c892b8c3a67270acf2ec4abcaf870
BigFos <jfreak0506@yahoo.com>
2017-05-20 20:43:43 -0700

    made some style changes

--- GifTastic/assets/javascript/app.js
+++ GifTastic/assets/javascript/app.js
@@ -45,7 +45,7 @@ function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < movies.length; i++) {
         var a = $("<button>");
-        a.addClass("movie");
+        a.addClass("movie btn btn-primary");
         a.attr("data-name", movies[i]);
         a.text(movies[i]);
         $("#buttons-view").append(a);
@@ -58,6 +58,7 @@ $("#add-movie").on("click", function(event) {
     movies.push(movie);
     console.log(movies)
     renderButtons();
+    $("#movie-input").val("");
 });
 
 $(document).on("click", ".movie", displayMovieInfo);
